4d30f992d3a67603724fcaa0a334f4a328c66cb6
jdm19p <jdm19p@fsu.edu>
2025-08-25 00:00:52 -0400

    good

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
         {children}
         <Footer />
-        <Analytics/> 
+        <Analytics />
       </body>
     </html>
   );
